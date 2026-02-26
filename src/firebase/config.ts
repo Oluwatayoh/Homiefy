d2d4af8ca4a32b738f233f3e9817011600103dda
Ojo Oluwatayo Michael <ojotayoh2@gmail.com>
2026-02-26 13:11:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7450371955-bb428",
+  "appId": "1:552025325896:web:68f75b87ef9f145dbb737d",
+  "apiKey": "AIzaSyBHfuqgNmNE5bQ3kAINW_Vuv8S3N6fbPaQ",
+  "authDomain": "studio-7450371955-bb428.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "552025325896"
+};
